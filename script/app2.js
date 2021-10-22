3f2649cbd604d3a5ca0f3e4e036b0bb59e35925e
faulanspurba <faulans.purba2@gmail.com>
2021-10-22 17:46:42 +0700

    deleting some files

--- script/app2.js
+++
@@ -1,6 +0,0 @@
-const span = document.querySelector("span");
-
-let blink_speed = 500; // every 1000 == 1 second, adjust to suit
-let t = setInterval(function () {
-  span.style.visibility = span.style.visibility == "hidden" ? "" : "hidden";
-}, blink_speed);
